944cd07668afe9213c78841f22e1ae86d5e7faa7
Amor <amor90s.zhang@gmail.com>
2018-07-11 09:30:11 +0800

    add twosum

---
+++ twoSum.go
@@ -0,0 +1,13 @@
+//Package twosum https://leetcode.com/problems/two-sum/description/
+package twosum
+
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[j] == target-nums[i] {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
