cec8687e9c206ccd69547d91e1eb9aff19be862c
Amor <amor90s.zhang@gmail.com>
2018-07-12 09:55:48 +0800

    update

--- twoSum.go
+++ twoSum.go
@@ -2,12 +2,18 @@
 package twosum
 
 func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
 	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[j] == target-nums[i] {
-				return []int{i, j}
-			}
+		m[nums[i]] = i
+	}
+	var ans []int
+	for i := 0; i < len(nums); i++ {
+		gap := target - nums[i]
+		if _, ok := m[gap]; ok && m[gap] != i {
+			ans = append(ans, i)
+			ans = append(ans, m[gap])
+			break
 		}
 	}
-	return nil
+	return ans
 }
